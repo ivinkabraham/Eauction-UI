815f0e7e4f00a9c215a8d9dd3fc9ff287f86af3e
Ivin <ivinkab@gmail.com>
2022-02-08 12:23:42 +0530

    changes

--- src/app/services/bid.service.ts
+++ src/app/services/bid.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -17,6 +17,7 @@ export class BidService {
   }
 
   getBidDetails(productId: string): Observable<any> {
-    return this.http.get(this.bidserviceUrl + productId);
+    const header=new HttpHeaders({"Ocp-Apim-Subscription-Key":"716ffdb13e604d07b4e18bc9bb3016db"});
+    return this.http.get(this.bidserviceUrl + productId,{headers:header});
   }
 }
