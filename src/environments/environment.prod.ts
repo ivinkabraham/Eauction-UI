e3cb4a9a1d4cbc24667121cbff7fe98991f2f811
Ivin <ivinkab@gmail.com>
2022-02-08 20:36:17 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  sellerbaseUrl:"https://eauctionapi.azure-api.net/sellermanagement/e-auction/api/v1/Seller/"
+  sellerbaseUrl:"https://eauctionapi.azure-api.net/sellermanagement/e-auction/api/v1/Seller/",
+  apimSubscriprionKey:"716ffdb13e604d07b4e18bc9bb3016db"
 };
